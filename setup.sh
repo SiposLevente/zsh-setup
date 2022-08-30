697bed11c143ea02de99bcf41951a3e410b41874
Sipos Levente <45292341+SiposLevente@users.noreply.github.com>
2022-08-30 13:22:27 +0000

    minor changes

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo `dirname $0`/.zshrc > $HOME/.zshrc
+cp `dirname $0`/.zshrc $HOME/.zshrc
