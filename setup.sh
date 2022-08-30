a5e8a8caf43c8ec22feb7d584896fd117ded0d6a
Sipos Levente <45292341+SiposLevente@users.noreply.github.com>
2022-08-30 12:53:22 +0000

    reordered commands

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+cp `dirname $0`/.zshrc $HOME
 cd $HOME
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp `dirname $0`/.zshrc $HOME
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
