9f15193f0b119edb85cc30fe0fc27869bfb1d668
Sipos Levente <45292341+SiposLevente@users.noreply.github.com>
2022-08-30 13:23:53 +0000

    minor update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+zsh_location=`dirname $0`/.zshrc`
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-cp `dirname $0`/.zshrc $HOME/.zshrc
+cp $zsh_location $HOME/.zshrc
