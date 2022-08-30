d881bd0644ab4e1fb6ba5f1ab077fa5d7e4d9e06
Sipos Levente <45292341+SiposLevente@users.noreply.github.com>
2022-08-30 13:17:30 +0000

    changed zshrc moving

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-cp `dirname $0`/.zshrc $HOME
-cd $HOME
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+echo `dirname $0`/.zshrc > $HOME/.zshrc
